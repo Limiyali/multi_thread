85ee07bf2d18656d1519ccf1a202cba6dfbd2538
lenosey <lenosey@foxmail.com>
2022-03-08 18:17:45 +0800

    [feat] add call once

--- 02_once_flag.cpp
+++ 02_once_flag.cpp
@@ -8,8 +8,52 @@ using namespace std;
 class A
 {
 public:
-    f() { cout << " A::f()\n"; }
+    void delay()
+    {
+        init = true;
+        this_thread::sleep_for(chrono::seconds(1));
+        flag = true;
+    }
+    void f()
+    {
+        if (flag)
+            cout << " A::f()\n";
+        else
+            cout << " error\n";
+    }
+    bool init = false;
+    bool flag = false;
 };
 
+A a;
 shared_ptr<A> p;
 once_flag flag;
+mutex m;
+
+void init()
+{
+    call_once(flag, [&]
+              { a.delay(); });
+    a.f();
+}
+
+void error_init()
+{
+    if (!a.init)
+    { // 未上锁，其他线程可能在执行 #1，则此时 p 不为空
+        std::lock_guard<std::mutex> l(m);
+        if (!a.init)
+        {
+            a.delay();
+        }
+    }
+    a.f(); // p 可能指向一块还未构造实例的内存，从而崩溃
+}
+
+int main()
+{
+    thread t(init);
+    thread t2(init);
+    t.join();
+    t2.join();
+}
